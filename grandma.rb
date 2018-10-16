f200f635c2a5a63cf452b6c01dec295346db8ec8
Anandha Sudhan <rasudhan@gmail.com>
2018-10-16 18:47:44 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -13,5 +13,11 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 def speak_to_grandma(phrase)
+  if phrase == phrase.upcase
+    return "NO, NOT SINCE 1938!"
+  elsif phrase == "I LOVE YOU GRANDMA!"
+    return "I LOVE YOU GRANDMA!"
+  else
+    return "I LOVE YOU TOO PUMPKIN!"
 
 end
